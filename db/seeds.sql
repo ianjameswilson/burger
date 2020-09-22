f5dfa507ffffb4a0b84ffa428c66664c64c659ce
Ian James Wilson <wilson.ian26@gmail.com>
2020-09-22 13:05:29 -0500

    added file structure

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+
+VALUES 
+    ("Original", 0),
+    ("Cheeseburger", 0),
+    ("Double Cheeseburger", 0)
+    ("Patty Melt on Texas Toast", 0),
+    ("Chili-Cheeseburger", 0);
